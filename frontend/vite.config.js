18ec5609ccf3eebc48f66085c453d7ddb6378bd6
Pankaj Kumar Lakhara <ipankajlakhara49@gmail.com>
2026-01-30 17:53:29 +0530

    git commit -m "Debug prod error: disable manualChunks"

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,25 +4,4 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('recharts')) return 'vendor-charts'
-            if (id.includes('lucide-react')) return 'vendor-icons'
-            if (
-              id.includes('react') ||
-              id.includes('react-dom') ||
-              id.includes('react-router-dom')
-            ) {
-              return 'vendor-core'
-            }
-            return 'vendor-others'
-          }
-        },
-      },
-    },
-    chunkSizeWarningLimit: 1000,
-  },
 })
