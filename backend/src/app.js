120c7708e02cc9d96682fca3f8d846c9b56a8f98
Pankaj Kumar Lakhara <ipankajlakhara49@gmail.com>
2026-01-30 16:18:06 +0530

    update cors config

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,7 @@ const app = express();
 // CORS Configuration
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
   }),
